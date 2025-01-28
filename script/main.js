ce30695f8268dbae2b5571fb3363c257667accb6
Evgesap <saparove9@gmail.com>
2025-01-28 22:24:26 +0300

    Добавление логики к страницу Кофейня

---
+++ script/main.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/* order */
+
+const goods = document.querySelectorAll(".goods-items__coffee-house");
+const checkboxBtn = document.querySelectorAll('[name="goods"]');
+
+let goodsData = [];
+
+checkboxBtn.forEach((checkbox, index) => {
+  checkbox.addEventListener("change", function () {
+    const choiceQuantity = document.querySelector(
+      `[data-name="${checkbox.value}"]`
+    );
+
+    if (checkbox.checked) {
+      choiceQuantity.setAttribute("value", 1);
+      goodsData.push({
+        name: checkbox.value,
+        price: checkbox.dataset.price,
+        quantity: parseInt(choiceQuantity.value),
+      });
+    } else {
+      goodsData.splice(goodsData.indexOf(goodsData[index]), 1);
+      choiceQuantity.setAttribute("value", 0);
+    }
+
+    console.log(goodsData);
+  });
+});
+
+/* result sum */
+
+const getResultSum = () => {};
+
+/* order data */
+
+const userData = {};
+
+const userSurname = document.querySelector('[name="surname"]');
+const userName = document.querySelector('[name="name"]');
+const result = document.querySelector(".sum__coffee-house");
+const btn = document.querySelector(".btn__coffee-house");
+
+btn.addEventListener("click", function () {
+  userData.surname = userSurname.value;
+  userData.name = userName.value;
+
+  alert(`Заказчик: ${userData.surname} ${userData.name}`);
+});
